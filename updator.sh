92e2bbabdfd5291659680524c4027f17783dfd5b
h5law <dev@h5law.com>
2024-04-20 17:02:54 +0100

    Copied files: updator.sh

--- updator.sh
+++ updator.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-GUM_SPIN_SHOW_OUTPUT=true # buggy for some reason
+export GUM_SPIN_SHOW_OUTPUT=true # buggy for some reason
 
-tools="brew cargo gh go rustup pkgx pip3 modular"
+tools="brew cargo gh go rustup foundryup pkgx pip3 modular"
 choices=""
 
 # check which tools are installed
 for tool in $tools; do
-    if command -v $tool &> /dev/null; then
+    if command -v "${tool}" &> /dev/null; then
         choices="$choices $tool"
     fi
 done
@@ -15,47 +15,51 @@ done
 # add --all option
 if [ "$1" != "--all" ]; then
     echo "Choose what packages you want to update: "
-    choices=$(gum choose $choices --no-limit)
+    choices=$(gum choose ${choices} --no-limit)
 fi
 
-for choice in $choices; do
-    if [ "$choice" = "rustup" ]; then
+for choice in ${choices}; do
+    if [ "${choice}" = "rustup" ]; then
         echo "Updating Rust toolchain..."
         cmd="rustup update"
-        gum spin -s minidot --title "Running '$cmd'" -- $cmd
-    elif [ "$choice" = "cargo" ]; then
+        gum spin -s minidot --title "Running '${cmd}'" -- "${cmd}"
+    elif [ "${choice}" = "cargo" ]; then
         echo "Updating Rust binaries..."
         cmd="cargo install-update -a"
-        gum spin -s minidot --title "Running '$cmd'" -- $cmd
-    elif [ "$choice" = "go" ]; then
+        gum spin -s minidot --title "Running '${cmd}'" -- "${cmd}"
+    elif [ "${choice}" = "foundryup" ]; then
+        echo "Updating Foundry toolchain..."
+        cmd="foundryup --arch arm64 --platform darwin"
+        gum spin -s minidot --title "Running '${cmd}'" -- "${cmd}"
+    elif [ "${choice}" = "go" ]; then
         if ! command -v go-global-update &> /dev/null; then
             cmd="go install github.com/gelio/go-global-update@latest"
-            gum spin -s minidot --title "Installing go-global-update..." -- $cmd
+            gum spin -s minidot --title "Installing go-global-update..." -- "${cmd}"
         fi
         echo "Updating Go binaries..."
         cmd="go-global-update"
         gum spin -s minidot --title "Running '$cmd'" -- $cmd
-    elif [ "$choice" = "brew" ]; then
+    elif [ "${choice}" = "brew" ]; then
         echo "Updating brew..."
         brew update
         brew upgrade
-        brew upgrade `brew list --cask`
+        brew upgrade $(brew list --cask)
         brew cleanup
         brew autoremove 
         brew doctor
-    elif [ "$choice" = "gh" ]; then
+    elif [ "${choice}" = "gh" ]; then
         echo "Updating gh extensions..."
         cmd="gh extension upgrade --all"
-        gum spin -s minidot --title "Running '$cmd'" -- $cmd
-    elif [ "$choice" = "pkgx" ]; then
+        gum spin -s minidot --title "Running '${cmd}'" -- "${cmd}"
+    elif [ "${choice}" = "pkgx" ]; then
         echo "Updating pkgx..."
-        pkgx --sync --verbose
-        pkgx --update --verbose
-    elif [ "$choice" = "pip3" ]; then
+        cmd="pkgx --sync && pkgx --update"
+        gum spin -s minidot --title "Running '${cmd}'" -- "${cmd}"
+    elif [ "${choice}" = "pip3" ]; then
         echo "Updating pip3..."
-        cmd='pip3 --disable-pip-version-check list --outdated --format=json | python3 -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))" | xargs -n1 pipx install'
-        gum spin -s minidot --title "Running pip3 updator" -- $cmd
-    elif [ "$choice" = "modular" ]; then
+        cmd="pip3 --disable-pip-version-check list --outdated --format=json | python3 -c \"import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))\" | xargs -n1 pipx install"
+        gum spin -s minidot --title "Running pip3 updator" -- "${cmd}"
+    elif [ "${choice}" = "modular" ]; then
         echo "Updating Modular Packages..."
         modular list-packages | awk '{print $1}' | xargs -I {} modular update {}
     fi
